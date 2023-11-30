041d9963bc39becd5c9865e2daad229f6348fe12
Viktor Nikolov <vnikolov@vnikolov.cz>
2023-11-30 20:17:35 +0100

    Update HW project

--- project_files/ArtyA7_MicroBlaze_demo_hw/ArtyA7_MicroBlaze_demo_hw.gen/sources_1/bd/system/hdl/system_wrapper.v
+++ project_files/ArtyA7_MicroBlaze_demo_hw/ArtyA7_MicroBlaze_demo_hw.gen/sources_1/bd/system/hdl/system_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2023.1 (win64) Build 3865809 Sun May  7 15:05:29 MDT 2023
-//Date        : Sat Nov 25 19:33:23 2023
+//Date        : Thu Nov 30 19:57:45 2023
 //Host        : Viktor-PC running 64-bit major release  (build 9200)
 //Command     : generate_target system_wrapper.bd
 //Design      : system_wrapper
