297fe5b56b625a413290e4d8c1457b04596d67f3
Viktor Nikolov <vnikolov@vnikolov.cz>
2024-06-09 18:06:22 +0200

    ArtyA7_MicroBlaze_demo_hw_2024.1 created

---
+++ project_files/ArtyA7_MicroBlaze_demo_hw_2024.1/ArtyA7_MicroBlaze_demo_hw.gen/sources_1/bd/system/hdl/system_wrapper.v
@@ -0,0 +1,97 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+//Date        : Sat Jun  8 19:51:53 2024
+//Host        : Viktor-PC running 64-bit major release  (build 9200)
+//Command     : generate_target system_wrapper.bd
+//Design      : system_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module system_wrapper
+   (CLK100MHZ,
+    ck_a0,
+    ck_rst,
+    ddr3_sdram_addr,
+    ddr3_sdram_ba,
+    ddr3_sdram_cas_n,
+    ddr3_sdram_ck_n,
+    ddr3_sdram_ck_p,
+    ddr3_sdram_cke,
+    ddr3_sdram_cs_n,
+    ddr3_sdram_dm,
+    ddr3_sdram_dq,
+    ddr3_sdram_dqs_n,
+    ddr3_sdram_dqs_p,
+    ddr3_sdram_odt,
+    ddr3_sdram_ras_n,
+    ddr3_sdram_reset_n,
+    ddr3_sdram_we_n,
+    usb_uart_rxd,
+    usb_uart_txd);
+  input CLK100MHZ;
+  output [0:0]ck_a0;
+  input ck_rst;
+  output [13:0]ddr3_sdram_addr;
+  output [2:0]ddr3_sdram_ba;
+  output ddr3_sdram_cas_n;
+  output [0:0]ddr3_sdram_ck_n;
+  output [0:0]ddr3_sdram_ck_p;
+  output [0:0]ddr3_sdram_cke;
+  output [0:0]ddr3_sdram_cs_n;
+  output [1:0]ddr3_sdram_dm;
+  inout [15:0]ddr3_sdram_dq;
+  inout [1:0]ddr3_sdram_dqs_n;
+  inout [1:0]ddr3_sdram_dqs_p;
+  output [0:0]ddr3_sdram_odt;
+  output ddr3_sdram_ras_n;
+  output ddr3_sdram_reset_n;
+  output ddr3_sdram_we_n;
+  input usb_uart_rxd;
+  output usb_uart_txd;
+
+  wire CLK100MHZ;
+  wire [0:0]ck_a0;
+  wire ck_rst;
+  wire [13:0]ddr3_sdram_addr;
+  wire [2:0]ddr3_sdram_ba;
+  wire ddr3_sdram_cas_n;
+  wire [0:0]ddr3_sdram_ck_n;
+  wire [0:0]ddr3_sdram_ck_p;
+  wire [0:0]ddr3_sdram_cke;
+  wire [0:0]ddr3_sdram_cs_n;
+  wire [1:0]ddr3_sdram_dm;
+  wire [15:0]ddr3_sdram_dq;
+  wire [1:0]ddr3_sdram_dqs_n;
+  wire [1:0]ddr3_sdram_dqs_p;
+  wire [0:0]ddr3_sdram_odt;
+  wire ddr3_sdram_ras_n;
+  wire ddr3_sdram_reset_n;
+  wire ddr3_sdram_we_n;
+  wire usb_uart_rxd;
+  wire usb_uart_txd;
+
+  system system_i
+       (.CLK100MHZ(CLK100MHZ),
+        .ck_a0(ck_a0),
+        .ck_rst(ck_rst),
+        .ddr3_sdram_addr(ddr3_sdram_addr),
+        .ddr3_sdram_ba(ddr3_sdram_ba),
+        .ddr3_sdram_cas_n(ddr3_sdram_cas_n),
+        .ddr3_sdram_ck_n(ddr3_sdram_ck_n),
+        .ddr3_sdram_ck_p(ddr3_sdram_ck_p),
+        .ddr3_sdram_cke(ddr3_sdram_cke),
+        .ddr3_sdram_cs_n(ddr3_sdram_cs_n),
+        .ddr3_sdram_dm(ddr3_sdram_dm),
+        .ddr3_sdram_dq(ddr3_sdram_dq),
+        .ddr3_sdram_dqs_n(ddr3_sdram_dqs_n),
+        .ddr3_sdram_dqs_p(ddr3_sdram_dqs_p),
+        .ddr3_sdram_odt(ddr3_sdram_odt),
+        .ddr3_sdram_ras_n(ddr3_sdram_ras_n),
+        .ddr3_sdram_reset_n(ddr3_sdram_reset_n),
+        .ddr3_sdram_we_n(ddr3_sdram_we_n),
+        .usb_uart_rxd(usb_uart_rxd),
+        .usb_uart_txd(usb_uart_txd));
+endmodule
